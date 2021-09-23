8a03d1a365269b99ab1aa9baefd1a3beaa006f88
DaveEdsonGit <60834760+DaveEdsonGit@users.noreply.github.com>
2021-09-22 22:51:15 -0700

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import PuzzleCanvas from "./puzzleCanvas";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = {};
+type AppState = {};
+
+export class App extends Component<AppProps, AppState>
+{
+    render()
+    {
+      return (
+        <div>
+          <p>Hello Jigsaw!</p>
+          <br />
+          <PuzzleCanvas />
+        </div>
+      );
+    }
 }
 
 export default App;
